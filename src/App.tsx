7a8563516eb34398aaa83dd272bd826fc57fd402
Jihyeon Kim <develop3357@gmail.com>
2022-12-07 21:25:21 +0900

    Upgrade React-router-dom to v6 from previous source code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { Outlet } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-weight: 300;
+    font-family: 'Source Sans Pro', sans-serif;
+    background-color:${(props) => props.theme.bgColor};
+    color:${(props) => props.theme.textColor};
+    line-height: 1.2;
+  }
+  a {
+    text-decoration:none;
+    color:inherit;
+  }
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <GlobalStyle />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import router from "./Router";
+import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
