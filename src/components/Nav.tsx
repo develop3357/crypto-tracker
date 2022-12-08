a750e907685d3baaac50e7672981a6f48f2786a2
Jihyeon Kim <develop3357@gmail.com>
2022-12-08 12:26:54 +0900

    Minor fix: nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,37 +1,50 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import styled, { ThemeContext, useTheme } from "styled-components";
 
 const NavContainer = styled.div`
   padding: 0px 20px;
   max-width: 480px;
+  height: 40px;
   margin: 0 auto;
-  button {
-    padding: 0 5px;
-    cursor: pointer;
-    outline: inherit;
-  }
 `;
 
-const NavBtns = styled.button`
-  font-size: 36px;
+const NavBtn = styled.button<{ right?: boolean }>`
+  font-size: 24px;
+  min-width: 40px;
+  min-height: 40px;
+  cursor: pointer;
+  outline: inherit;
+  float: ${(props) => (props.right ? "right" : "left")};
 `;
-const BackBtn = styled(NavBtns)``;
-const HomeBtn = styled(NavBtns)`
-  float: right;
-`;
+
+const theme = "dark";
 
 function Nav() {
   const navigate = useNavigate();
+  const themeContext = useContext(ThemeContext);
+  const ut = useTheme();
+  const [darkMode, setDarkMode] = useState(true);
+  // themeContext.bgColor = "white";
   const onBackBtnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     navigate(-1);
   };
+  const toggleThemeBtnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    // themeContext.bgColor = "white";
+    // themeContext.textColor = "red";
+    // setDarkMode((darkMode) => !darkMode);
+    console.log(themeContext);
+    console.log(ut);
+  };
   return (
     <NavContainer>
-      <BackBtn onClick={onBackBtnClick}>🔙</BackBtn>
-      <HomeBtn>
+      <NavBtn onClick={onBackBtnClick}>🔙</NavBtn>
+      <NavBtn right>
         <Link to="/">🏠</Link>
-      </HomeBtn>
+      </NavBtn>
+      <NavBtn right onClick={toggleThemeBtnClick}>
+        {theme === "dark" ? "☼" : "☾"}
+      </NavBtn>
     </NavContainer>
   );
 }
